dc113e9684e08f4464751a9e4971b530f8a4a0e1
eowdeveloper <madeline.eow@gmail.com>
2023-02-09 21:35:34 -0500

    img edited

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -28,7 +28,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
           className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
         >
           <img
-            src="./public/headset.svg"
+            src="/headset.svg"
             alt="headset"
             className="w-1/2 h-1/2 object-contain"
           />
